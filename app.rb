b4d77ba85cab99f70f4a325f6084f590956eb4c9
Robby Ranshous <rranshous@gmail.com>
2016-01-14 14:32:06 -0500

    initial

---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'sinatra'
+
+get '/' do
+  '<html><body style="text-align:center; background-color:white;"><br/>codeflaw<br/><br/><img src="http://imgs.xkcd.com/comics/compiler_complaint.png"/></body></html>'
+end
